56c3fda7fc38cc5cf077817984a2eabd412ecd8d
DaffaHavez <daffa.havez00@gmail.com>
2024-11-26 15:38:22 +0400

    initial commit

---
+++ products.js
@@ -0,0 +1,47 @@
+let products = [
+    {
+        "id":1001,
+        "title":"Cat Food, 25lb bag",
+        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
+        "price":20.00,
+        "image": "images/catfood.jpg",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1002,
+        "title":"Cat ball, 25lb bag",
+        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
+        "price":20.00,
+        "image": "images/catball.jpg",
+        "availableInventory":11,
+        "rating":5
+    },
+    {
+        "id":1003,
+        "title":"Cat House, 25lb bag",
+        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
+        "price":20.00,
+        "image": "images/cathouse.jpg",
+        "availableInventory":20,
+        "rating":4
+    },
+    {
+        "id":1004,
+        "title":"Cat bell, 25lb bag",
+        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
+        "price":20.00,
+        "image": "images/catbell.jpg",
+        "availableInventory":10,
+        "rating":1
+    },
+    {
+        "id":1005,
+        "title":"Cat Food, 25lb bag",
+        "description": "A 25 pound of bag of <em>irresistible</em>, organic goodness for you",
+        "price":20.00,
+        "image": "images/catlitter.jpg",
+        "availableInventory":5,
+        "rating":3
+    }
+]   
